42e128e340a5386be1c3f03a63651e38987d8ce6
hieuht2203 <hieuht2203@gmail.com>
2026-06-11 19:51:25 +0700

    chore: add default secrets.h template with empty credentials

---
+++ src/secrets.h
@@ -0,0 +1,9 @@
+/**
+ * WiFi Credentials
+ *
+ * Copy this file to secrets.h and fill in your WiFi credentials.
+ * secrets.h is gitignored and will not be committed.
+ */
+
+#define WIFI_SSID ""
+#define WIFI_PASS ""
